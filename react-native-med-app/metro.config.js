16e67685e10ddd6db9d857b9effbbae8b6ba7fcf
Moz <96695085+eXemoumen@users.noreply.github.com>
2026-01-30 00:48:26 +0100

    feat: Set up Babel and Metro configurations for the mobile app, enabling NativeWind and module aliases.

--- react-native-med-app/metro.config.js
+++ react-native-med-app/metro.config.js
@@ -1,28 +1,6 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
-const path = require("path");
 
 const config = getDefaultConfig(__dirname);
-
-// Configure resolver for path aliases
-config.resolver = {
-  ...config.resolver,
-  // Ensure src directory is included in nodeModulesPaths for @ alias resolution
-  nodeModulesPaths: [
-    ...(config.resolver?.nodeModulesPaths || []),
-    path.resolve(__dirname, "src"),
-  ],
-  // Extra node modules to watch
-  extraNodeModules: {
-    ...config.resolver?.extraNodeModules,
-    "@": path.resolve(__dirname, "src"),
-  },
-};
-
-// Configure watchFolders to include src
-config.watchFolders = [
-  ...(config.watchFolders || []),
-  path.resolve(__dirname, "src"),
-];
 
 module.exports = withNativeWind(config, { input: "./global.css" });
